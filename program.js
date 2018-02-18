017fde18d73cf6cefe72a7e2bc41faf760d988b7
murat aka <murat-aka@hotmail.com>
2018-02-18 21:53:07 +0000

    exercise3

---
+++ program.js
@@ -0,0 +1,50 @@
+//////////////////////////////////////////
+/* this program contains exercises from */
+/* freecodecamps mongodb tutorials      */
+//////////////////////////////////////////
+
+
+         //////////////////
+         ///// START  /////
+         //////////////////
+
+
+
+
+ /*////////////////////////////////*/
+ /* function find exercise 3       */
+ /*////////////////////////////////*/
+
+ function find(){
+
+    var mongo = require('mongodb').MongoClient; // load mongo module
+    var url = "mongodb://localhost:27017/learnyoumongo";
+    
+    var limit = process.argv[2]; // paramater to find
+    
+    mongo.connect(url, function(err, dbase) {
+        
+        if(!err){
+            
+            dbase.collection('parrots').find({ 
+              age: { $gt : +limit } // greater than
+            }).toArray(function(err, documents) {
+                
+                if(!err){
+                    console.log(documents); 
+                }
+                
+            });            
+            
+        }
+        
+        dbase.close(); // close connection
+
+    });
+ 
+ }
+ 
+ find();
+ 
+ 
+
