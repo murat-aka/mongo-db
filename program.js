25f7bf985e70fa3eb36ebceb74bc73b05665a087
murat aka <murat-aka@hotmail.com>
2018-02-20 23:18:01 +0000

    exercise4

--- program.js
+++ program.js
@@ -44,7 +44,48 @@
  
  }
  
- find();
+ //find();
  
  
+ 
+ /*////////////////////////////////*/
+ /* function findDoc exercise 4    */
+ /*////////////////////////////////*/
+
+ function findDoc(){
+
+    var mongo = require('mongodb').MongoClient; // load mongo module
+    var url = "mongodb://localhost:27017/learnyoumongo";
+    
+    var limit = process.argv[2]; // paramater to find
+    
+    mongo.connect(url, function(err, dbase) {
+        
+        if(!err){
+            
+            dbase.collection('parrots').find({ 
+                age: {
+                  $gt : +limit  // greater than
+                }
+              }, {
+                name: 1,
+                age: 1,
+                _id: 0
+            }).toArray(function(err, documents) {
+                
+                if(!err){
+                    console.log(documents); 
+                }
+                
+            });            
+            
+        }
+        
+        dbase.close(); // close connection
+
+    });
+ 
+ }
+ 
+ findDoc();
 
